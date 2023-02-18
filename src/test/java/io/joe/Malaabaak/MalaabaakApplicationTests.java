1b8a61584db0f88b181280628ac4b6f887ef69a1
Youssif Essam <yessam@citglobal.com>
2023-02-18 17:24:13 +0200

    Initial commit

---
+++ src/test/java/io/joe/Malaabaak/MalaabaakApplicationTests.java
@@ -0,0 +1,13 @@
+package io.joe.Malaabaak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalaabaakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
